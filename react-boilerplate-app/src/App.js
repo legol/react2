edbe98faff7522fef2eefacd25072294fb6c2fd1
ubuntu <legol.chen@gmail.com>
2019-12-23 00:57:59 -0800

    call server (cross site)

--- react-boilerplate-app/src/App.js
+++ react-boilerplate-app/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios');
+
 function App() {
+  testServerCall();
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +24,24 @@ function App() {
       </header>
     </div>
   );
+}
+
+
+function testServerCall() {
+
+  // Make a request for a user with a given ID
+  axios.get('http://192.168.101.202:20010/index.php?test=abcd')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 }
 
 export default App;
